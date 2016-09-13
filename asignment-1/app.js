ea58ead1aa64b786a5274c72e19a7501a947ca37
thopa <thomas.pawlitzki@gmail.com>
2016-09-13 18:18:13 +0200

    implemented color changing

--- asignment-1/app.js
+++ asignment-1/app.js
@@ -3,38 +3,31 @@
 // Define the module
 var module = angular.module('LunchCheck', []);
 module.controller('LunchCheckController', ['$scope', function($scope) {
-	
-	
+
+
 	var msg_empty = "Please enter data first";
 	var msg_ok = "Enjoy!";
 	var msg_too_much = "Too much!";
-	
+
 	var max_amount = 3;
-	
-	
-	$scope.verifyItems = function() {
-		if($scope.items) {
+
+	$scope.message = "";
+	$scope.messageClass = "";
+
+	$scope.check = function(items) {
+		if(items) {
 			var numberOfItems = $scope.items.split(",").filter(function(element) {
 				return element && element.trim().length > 0;
 			}).length;
 			if(numberOfItems <= max_amount) {
-				return true;				
-			}
-		}
-		return false;
-	}	
-		
-	$scope.message = "";
-	
-	$scope.check = function() {
-		if($scope.items) {
-			if($scope.verifyItems()) {
 				$scope.message = msg_ok;
 			} else {
 				$scope.message = msg_too_much;
-			}			
+			}
+			$scope.messageClass = "green";
 		} else {
 			$scope.message = msg_empty;
-		}		
+			$scope.messageClass = "red";
+		}
 	};
 }]);

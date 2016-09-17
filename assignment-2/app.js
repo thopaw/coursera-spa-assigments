8b20cd657f6e3e85b0f9d55f2552560b0d752d71
thopa <thomas.pawlitzki@gmail.com>
2016-09-17 12:35:32 +0200

    solution of assignment-2 & crected typo in asignment-1 directory

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -1,38 +1,74 @@
 'use strict';
 (function (){
-	
+
 	// Define the Module
 	var module = angular.module('ShoppingListCheckOff',[]);
-	
+
 	// Include the Controllers
 	module.controller('ToBuyShoppingController', ToBuyShoppingController);
 	module.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController);
-	
-	// Define the Services
-	module.factory('items', ItemsService);
+	// Include Service
+	module.service('ItemsService', ItemsService);
 
-	// Define Controller	
-	ToBuyShoppingController.$inject = ['$scope', 'items'];
-	function ToBuyShoppingController($scope, items) {
-		$scope.name = "Test";
-		
-		$scope.getItems = function() {
-			return items.itemsToBuy;
-		};
-		
+	// Define Controller
+	ToBuyShoppingController.$inject = ['ItemsService'];
+	function ToBuyShoppingController(ItemsService) {
+		var toBuy = this;
+
+		toBuy.items = ItemsService.getItemsToBuy();
+
+		toBuy.buyItem = function(index) {
+			ItemsService.buyItem(index);
+		}
+
+		toBuy.newItem = function() {
+			ItemsService.addItem(toBuy.newQuantity, toBuy.newName);
+			toBuy.newQuantity = undefined;
+			toBuy.newName = undefined;
+		}
+
 	};
-	
-	// Define Controller	
-	AlreadyBoughtShoppingController.$inject = ['$scope', 'items'];
-	function AlreadyBoughtShoppingController($scope, items) {
+
+	// Define Controller
+	AlreadyBoughtShoppingController.$inject = ['ItemsService'];
+	function AlreadyBoughtShoppingController(ItemsService) {
+		var bought = this;
+
+		bought.items = ItemsService.getBoughtItems()
 	};
-	
+
 	// Define Service
-	ItemsService.$inject = [];
 	function ItemsService() {
-		return function() {
-			var itemsToBuy = [ { name: 'cookies', quantity: 10 }, { name: 'milk', quantity: 3 } ];
-			var itemsBought = [];
+		var service = this;
+
+		var itemsToBuy = [
+			{ name: 'cookies', quantity: 10 },
+			{ name: 'milk', quantity: 3 },
+			{ name: 'apple', quantity: 4 },
+			{ name: 'banana', quantity: 7 },
+			{ name: 'grapes', quantity: 1 },
+			{ name: 'bread', quantity: 2 }
+	  ];
+		var itemsBought = [];
+
+		service.getItemsToBuy = function() {
+			return itemsToBuy;
+		};
+
+		service.addItem = function(quantity, name) {
+			if(quantity && name) {
+				itemsToBuy.push({ name: name, quantity: quantity});
+			}
+		}
+
+		service.buyItem = function (index) {
+			var boughtItem = itemsToBuy[index];
+			itemsBought.push(itemsToBuy[index]);
+			itemsToBuy.splice(index,1);
+		};
+
+		service.getBoughtItems = function() {
+			return itemsBought;
 		};
 	};
 
